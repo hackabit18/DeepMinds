006535b1690e0a7c066957677e6c25c105ff4274
Kevin Mathew T <mathewtkevin@gmail.com>
2018-10-27 03:41:22 +0530

    Webhook needs debugging

--- Webhook/bot_reply.py
+++ Webhook/bot_reply.py
@@ -1,22 +1,32 @@
-import keras
-import pandas as pd
-import numpy as np
 import pickle
-from flask import Flask, request, make_response, jsonify
+import keras
+import numpy as np
 import tensorflow as tf
+import pandas as pd
+from flask import Flask, request, make_response, jsonify
+from collections import OrderedDict
 app = Flask(__name__)
+
+with open('../model/SymptomsAndDiseasesList.pickle', 'rb') as f:
+    _, disease_list = pickle.load(f)
+
+symptoms_list = np.array(pd.read_csv('../datasets/Training.csv').columns.values)[:-1]
+# for i in range(len(symptoms_list)):
+#     symptoms_list[i] = symptoms_list[i].replace('_', ' ')
+
+with open('../datasets/DiseaseToSymptomsDictionary.pickle', 'rb') as handle:
+    symp = pickle.load(handle)
 
 
 class DiffSessions:
-    global graph
+    global symptoms_list, symp
     graph = tf.get_default_graph()
-    model = keras.models.load_model('../weights/disease_predictor.h5')
+    model = keras.models.load_model('../model/DiseasePredictor.h5')
     print(model.summary())
     threshold = 0.65
     current_symptoms_yes = []
     current_symptoms_no = []
-    with open('SymptomsAndDiseasesList.pickle', 'wb') as f:
-        symptoms_list, disease_list = pickle.load(f)
+
     symptom2vec = {}
     vec2symptom = {}
     all_possible_diseases = []
@@ -25,8 +35,10 @@ class DiffSessions:
     current_disease_list = []
     query = ""
     last_asked = ""
-    with open('../datasets/symp.pickle', 'rb') as handle:
-        symp = pickle.load(handle)
+    thres = 4
+    new_map = {}
+    current_disease_list_local = []
+    c = 0
 
     for i in range(len(symptoms_list)):
         symptom2vec[symptoms_list[i]] = i
@@ -36,16 +48,16 @@ class DiffSessions:
         symptoms_return = []
         for iterate in range(len(one_hot)):
             if one_hot[iterate] == 1:
-                symptoms_return.append([self.symptoms_list[iterate]])
+                symptoms_return.append([symptoms_list[iterate]])
 
         return symptoms_return
 
     def getOneHot(self, symptoms):
         # print(symptoms)
         one_hot = []
-        for i in range(len(self.symptoms_list)):
-            # print(self.symptoms_list[i])
-            if self.symptoms_list[i] in symptoms:
+        for i in range(len(symptoms_list)):
+            # print(symptoms_list[i])
+            if symptoms_list[i] in symptoms:
                 one_hot.append(1)
             else:
                 one_hot.append(0)
@@ -65,8 +77,8 @@ class DiffSessions:
         if 'action' not in data['queryResult']:
             self.addToSymptomsList(data['queryResult']['parameters']['medical'], [])
             self.getPrediction()
-            self.last_asked, query = self.ask_question()
-            print(self.last_asked, query)
+            self.last_asked, self.query = self.ask_question()
+            print(self.last_asked, self.query)
 
         else:
             print(data['queryResult']['action'])
@@ -81,9 +93,9 @@ class DiffSessions:
             print(self.current_symptoms_yes)
             print(self.current_symptoms_no)
 
-            self.last_asked, query = self.ask_question()
+            self.last_asked, self.query = self.ask_question()
 
-        return self.last_asked, query
+        return self.last_asked, self.query
 
         # if data['queryResult']['action'] == "MedicalConversation.MedicalConversation-yes":
         #     current_symptoms_yes.append(self.last_asked)
@@ -93,74 +105,73 @@ class DiffSessions:
         # addToSymptomsList(symptoms, [])
 
     def getPrediction(self):
+        self.c = 0
+        # print(symptoms_list, "---------")
         # print(self.current_symptoms_yes)
-        inputs = np.array(self.getOneHot(self.current_symptoms_yes)).reshape(-1, 133).astype('float32')
-        # print(inputs)
-        with graph.as_default():
+        inputs = np.array(self.getOneHot(self.current_symptoms_yes)).reshape(-1, 132).astype('float32')
+        print(inputs)
+        with self.graph.as_default():
             pred = self.model.predict(inputs, batch_size=64)
 
         # if max(pred.all()) >= threshold:
         # print(pred.shape)
-        new_map = {}
         for iterate in range(len(pred[0])):
-            new_map[pred[0][iterate]] = self.disease_list[iterate]
+            self.new_map[pred[0][iterate]] = disease_list[iterate]
 
-        # print(new_map)
-        c = 0
-        for key in sorted(new_map.keys(), reverse=True):
+        # print(self.new_map)
+        for key in sorted(self.new_map.keys(), reverse=True):
             # print("74982738497")
-            if new_map[key] not in self.current_disease_list:
-                self.current_disease_list.append(new_map[key])
-            print(key, new_map[key])
-            # all_possible_symptoms.append(new_map[key])
-            c += 1
-            if c == self.number_of_top_diseases:
+            if self.new_map[key] not in self.current_disease_list:
+                self.current_disease_list.append(self.new_map[key])
+                print(key, self.new_map[key], "--")
+                # all_possible_symptoms.append(self.new_map[key])
+                self.c += 1
+            if self.c > self.number_of_top_diseases:
                 break
 
-        print(self.current_disease_list)
+        print(self.current_disease_list, "---")
 
     def ask_question(self):
         # global self.current_disease_list, self.current_symptoms_yes, self.current_symptoms_no, symp
         x = len(self.current_symptoms_yes)
-        threshold = 4
-        while x < threshold:
+        while x < self.thres:
             for i in self.current_disease_list:
-                for j in self.symp[i]:
+                print(symp[i], "-----")
+                for j in symp[i]:
+
                     if (j in self.current_symptoms_yes) or (j in self.current_symptoms_no):
                         continue
                     else:
-                        x += 1
-                        ask = "Do you also have " + j + "?"
+                        # x += 1
+                        ask = "Do you also have " + j.replace("_", " ") + "?"
                         print(j, ask)
                         return j, ask
 
         else:
             # dise =
             # print(self.current_symptoms_yes)
-            inputs = np.array(self.getOneHot(self.current_symptoms_yes)).reshape(-1, 133).astype('float32')
+            inputs = np.array(self.getOneHot(self.current_symptoms_yes)).reshape(-1, 132).astype('float32')
             # print(inputs)
-            with graph.as_default():
+            with self.graph.as_default():
                 pred = self.model.predict(inputs, batch_size=64)
 
             # if max(pred.all()) >= threshold:
             # print(pred.shape)
-            new_map = {}
+            self.new_map = {}
             for iterate in range(len(pred[0])):
-                new_map[pred[0][iterate]] = self.disease_list[iterate]
+                self.new_map[pred[0][iterate]] = disease_list[iterate]
 
-            # print(new_map)
-            c = 0
-            current_disease_list_local = []
-            for key in sorted(new_map.keys(), reverse=True):
+            # print(self.new_map)
+            for key in sorted(self.new_map.keys(), reverse=True):
                 # print("74982738497")
-                if new_map[key] not in current_disease_list_local:
-                    current_disease_list_local.append(new_map[key])
-                print(key, new_map[key])
-                # all_possible_symptoms.append(new_map[key])
-                c += 1
-                if c == 1:
+                if self.new_map[key] not in self.current_disease_list_local:
+                    self.current_disease_list_local.append(self.new_map[key])
+                print(key, self.new_map[key])
+                # all_possible_symptoms.append(self.new_map[key])
+                self.c += 1
+                if self.c == 1:
                     break
-            return "done", "You maybe suffering from " + current_disease_list_local[0] + "."
+            return "done", "You maybe suffering from " + self.current_disease_list_local[0] + "."
 
             # receive =
 
@@ -193,10 +204,10 @@ def result():
         iterator += 1
         object_list.append(DiffSessions())
 
-    exit_or_not, query = object_list[object_dict[obj_name]].get_result(data)
+    exit_or_not, reply = object_list[object_dict[obj_name]].get_result(data)
 
-    query = query.replace("_", " ")
-    dictionary = {'fulfillmentText': query}
+    reply = reply.replace("_", " ")
+    dictionary = {'fulfillmentText': reply}
     # print(data['queryResult']['outputContexts']['lifespanCount'])
     # dictionary['outputContexts'] = data['queryResult']['outputContexts']
     print(len(object_list))
