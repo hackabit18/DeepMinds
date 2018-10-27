0e0ad415b52dfbea36208bfed7c5fc3e33968d6f
Rishabh Jain <jrishabh98@gmail.com>
2018-10-27 18:44:02 +0530

    Done

--- Google Custom Search API/my_code.py
+++ Google Custom Search API/my_code.py
@@ -1,21 +1,26 @@
 
 
-"""Simple command-line example for Custom Search.
-Command-line application that does a search.
-"""
+# """Simple command-line example for Custom Search.
+# Command-line application that does a search.
+# """
 
 
-import pprint
+# import pprint
 
 from googleapiclient.discovery import build
 
-
+# query - take output of DialogFlow
+query = 'Narendra Modi'
 service = build("customsearch", "v1",
             developerKey="AIzaSyDPg23pWoQC6bzMh0g5P37XUKyPdih_jXI")
 
 res = service.cse().list(    # pylint: disable=no-member
-      q='lectures',
-      cx='017576662512468239146:omuauf_lfve',
+      q= query,
+      cx='001132580745589424302:jbscnf14_dw',
     ).execute()
-pprint.pprint(res)
-
+# pprint.pprint(res)
+# print(len(res.items()))
+# print(len(res['items']))
+print('The top results from web are: ')
+for i in range(0,len(res['items'])):
+    print(res['items'][i]['link'])
