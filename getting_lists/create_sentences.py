bfb5fdc0e03315ab2e5261ec340986f4ce5d5790
Kevin Mathew T <mathewtkevin@gmail.com>
2018-10-27 16:13:49 +0530

    Fixed major errors

--- getting_lists/create_sentences.py
+++ getting_lists/create_sentences.py
@@ -3,7 +3,7 @@ f = open('C:\\Users\\Rishabh\\Desktop\\HackABIT\\DeepMinds\\getting_lists\\Sympt
 lines = f.split('\n')
 
 ndata = open('C:\\Users\\Rishabh\\Desktop\\HackABIT\\DeepMinds\\getting_lists\\Sentences List.txt','w+',encoding = 'utf-8')
-for i in range(0,len(lines)):
+for i in range(0, len(lines)):
     ndata.write('I am having '+ lines[i])
     ndata.write('\n')
 ndata.close()
